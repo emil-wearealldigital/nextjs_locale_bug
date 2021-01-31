59d18ee36e5a7c1eb1ddc8aac05f435231adc62c
Emil <emil.ralev@orbisagency.com>
2021-01-31 17:40:03 +0200

    Add more examples

--- pages/[param].js
+++ pages/[param].js
@@ -1,3 +1,19 @@
-export default function Page() {
+import {useRouter} from "next/router";
+
+export default function Page(props) {
+    const router = useRouter();
+
+    // Check node.js console when getServerSideProps is uncommented
+    console.log(router.pathname);
+    // You can also try throwing an exception here, it shows very well...
+
     return 'This page should be shown only if the url is /{locale}/[param], otherwise it\'s a bug.';
 }
+
+// export async function getServerSideProps(context) {
+//     return {
+//         props: {
+//             data: []
+//         },
+//     }
+// }
--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,29 @@ export default function Home() {
         }, `${router.pathname}?bug=true`, {locale: getLocale()});
     }
 
-    return <div style={{textAlign: 'center'}}>
+    return <div style={{textAlign: 'center', width: '500px', margin: 'auto'}}>
+        <br/><br/>
+
+        <p>{`If we are on this page and the url is /{locale}/[param] - this is a bug.`}</p>
+        <br/><br/>
+
         <p>
-            <button onClick={handleClick} style={{width: '400px'}}>Click here until the locale changes 2-3 times and then click "GO BACK" arrow
+            <button onClick={handleClick}>Click here until the locale changes 2-3 times and then click "GO BACK" arrow
                 in the browser (I think only happens with ?query)
             </button>
         </p>
+        <br/><br/>
 
         <p>
-            <button onClick={handleClick2} style={{width: '400px'}}>Click here and you can see that it's able to circumvent localization (possibly not a bug)</button>
+            <button onClick={handleClick2}>Click here and you can see that it's able to circumvent localization (possibly not a bug)</button>
         </p>
+        <br/><br/>
 
-        <p>{`If we are on this page and the url is /{locale}/[param] - this is a bug.`}</p>
+        <p>New bug: Go and uncomment getServerSideProps in [param].js, then click this button (it's the same as before).
+            It actually executes [param].js code without rendering it!
+        </p>
+        <p>
+            <button onClick={handleClick2}>If getServerSideProps is uncommented - component will execute code but not render when clicking here.</button>
+        </p>
     </div>;
 }
