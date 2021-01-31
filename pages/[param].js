a26937d5f25a320b2ce58c6da8262459c05ab195
Emil <emil.ralev@orbisagency.com>
2021-01-31 16:07:52 +0200

    Add bug examples

---
+++ pages/[param].js
@@ -0,0 +1,3 @@
+export default function Page() {
+    return 'This page should be shown only if the url is /{locale}/[param], otherwise it\'s a bug.';
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,33 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {useRouter} from "next/router";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const router = useRouter();
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const getLocale = () => {
+        return ['fr', 'de'][Math.round(Math.random())];
+    }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+    const handleClick = () => {
+        router.push('/?bug=true', null, {locale: getLocale()});
+    }
+
+    const handleClick2 = () => {
+        router.push({
+            pathname: `${router.pathname}?bug=true`,
+        }, `${router.pathname}?bug=true`, {locale: getLocale()});
+    }
+
+    return <div style={{textAlign: 'center'}}>
+        <p>
+            <button onClick={handleClick}>Click here until the locale changes 2-3 times and then click "GO BACK" arrow
+                in the browser
+            </button>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <p>
+            <button onClick={handleClick2}>CLICK HERE AND YOU CAN SEE THAT IT'S ABLE TO CIRCUMVENT LOCALIZATION</button>
+        </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <p>{`If we are on this page and the url is /{locale}/[param] - this is a bug.`}</p>
+    </div>;
 }
