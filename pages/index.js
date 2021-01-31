6b663f04a202cba2a933a2b044d9cb73433c9205
Emil <emil.ralev@orbisagency.com>
2021-01-31 17:20:20 +0200

    Add more explanation

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,13 @@ export default function Home() {
 
     return <div style={{textAlign: 'center'}}>
         <p>
-            <button onClick={handleClick}>Click here until the locale changes 2-3 times and then click "GO BACK" arrow
-                in the browser
+            <button onClick={handleClick} style={{width: '400px'}}>Click here until the locale changes 2-3 times and then click "GO BACK" arrow
+                in the browser (I think only happens with ?query)
             </button>
         </p>
 
         <p>
-            <button onClick={handleClick2}>CLICK HERE AND YOU CAN SEE THAT IT'S ABLE TO CIRCUMVENT LOCALIZATION</button>
+            <button onClick={handleClick2} style={{width: '400px'}}>Click here and you can see that it's able to circumvent localization (possibly not a bug)</button>
         </p>
 
         <p>{`If we are on this page and the url is /{locale}/[param] - this is a bug.`}</p>
